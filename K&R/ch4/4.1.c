b2a62d67ce8abe25bb73bafa92953784741499b9
TihonERA <miha311008@gmail.com>
2026-07-19 16:55:18 +0300

    Решил задачу 4.1.c, время: 1-2 минуты

---
+++ K&R/ch4/4.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int strrindex(char s[], char t[])
+{
+	int i, j, k, start;
+	
+	start = -1;
+	for (i = 0; s[i] != '\0'; i++) {
+		for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++) {
+			;
+		}
+		if (k > 0 && t[k] == '\0') {
+			start = j;
+		}
+	}
+	return start;
+}
